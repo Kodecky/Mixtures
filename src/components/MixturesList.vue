8fd8a3b9841b0ef0b4bb1badbb09b215074fdb89
Damian <paweladamian@gmail.com>
2024-05-23 19:23:02 +0200

    init commit

---
+++ src/components/MixturesList.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- mixtures -->
+  <ul class="mixtures">
+    <!-- <mixture-item
+      :key="index"
+      v-for="(mixture, index) in mixtures"
+      :mixture="mixture"
+      @increment="$emit('increment', index)"
+      @decrement="$emit('decrement', index)" /> -->
+    <flask-item
+      :key="index"
+      v-for="(mixture, index) in mixtures"
+      :variant="mixture.variant"
+      :amount="mixture.amount"
+      @increment="$emit('increment', index)"
+      @decrement="$emit('decrement', index)" />
+  </ul>
+</template>
+
+<script>
+// import MixtureItem from './MixtureItem'
+import FlaskItem from './shared/FlaskItem'
+
+export default {
+  name: 'Mixtures',
+  props: {
+    mixtures: {
+      type: Array,
+      required: true
+    }
+  },
+  components: { 
+    // MixtureItem 
+    FlaskItem
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.mixtures {
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 0;
+}
+</style>
---
+++ src/components/ResultBox.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+
+    <!-- text between -->
+    <p
+      v-text="'And the result...'" />
+
+    <flask-item
+      :buttons-visible="false"
+      :size="15"
+      :amount="100"
+      :color="mixtureEffectFill"
+      style="margin: 3rem auto; "/>
+
+    <button-item
+      @click="$emit('refresh')"
+      :size="4"
+      :movement="-0.5"
+      :font-size="1.5"
+      icon="sync" />
+
+
+  </div>
+</template>
+
+<script>
+// import BigMixtureItem from './BigMixtureItem'
+import ButtonItem from './shared/ButtonItem'
+import FlaskItem from './shared/FlaskItem'
+
+export default {
+  name: 'ResultsBox',
+  props: {
+    mixtures: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    mixtureEffectFill () {
+      const [redCol, greenCol, blueCol] = this.mixtures.map(item => Math.floor(item.amount * 2.5))
+      return `rgb(${redCol}, ${greenCol}, ${blueCol})`
+    }
+  },
+  components: {
+    // BigMixtureItem,
+    ButtonItem,
+    FlaskItem
+  }
+}
+</script>
